97b1df3853bd01f30597bceb424e056a568ac616
Brian Widtmann <>
2017-11-16 14:04:11 +0100

    Folder structure repaired
    Git folder was too deep and not inside root folder.

---
+++ TM-Multiplier/Multiplier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_Multiplier
+{
+    public class Multiplier
+    {
+        private IEnumerable<IEnumerable<int>> tapes;
+
+        public Multiplier(string tapeContent)
+        {
+            this.tapes = new List<IEnumerable<int>>();
+        }
+
+        private bool CheckTapeContent()
+        {
+            return false;
+        }
+    }
+}
