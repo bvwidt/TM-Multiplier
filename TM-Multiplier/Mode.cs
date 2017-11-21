cf06e1eb34f1f91571c136a2450988f60728ec08
Brian Widtmann <widtmbri@students.zhaw.ch>
2017-11-21 23:32:36 +0100

    Mode enum added to differ between result and step mode

---
+++ TM-Multiplier/Mode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_Multiplier
+{
+    public enum Mode
+    {
+        Result,
+        Step
+    }
+}
